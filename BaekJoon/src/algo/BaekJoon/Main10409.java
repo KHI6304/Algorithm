035539b09686f541404c7910d38b5206d82a7e88
김현일 <kimhi6304@naver.com>
2019-07-18 22:26:46 +0900

    서버

---
+++ BaekJoon/src/algo/BaekJoon/Main10409.java
@@ -0,0 +1,32 @@
+package algo.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main10409 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		int sum = 0;
+		int cnt = 0;
+		while(cnt < arr.length) {
+			sum += arr[cnt];
+			if(sum > T)
+				break;
+			cnt++;
+		}
+		
+		System.out.println(cnt);
+	}
+}
