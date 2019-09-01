53fa6bf8c35dc8b2c99ed204e849f62725272a7a
김현일 <kimhi6304@naver.com>
2019-09-01 19:12:39 +0900

    연산자 끼워넣기

--- BaekJoon/src/algo/BaekJoon/Main14888.java
+++ BaekJoon/src/algo/BaekJoon/Main14888.java
@@ -11,96 +11,88 @@ public class Main14888 {
 		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
-		nums = new int[N];
-		
+		arr = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++)
-			nums[i] = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < arr.length; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
 		
-		// 덧셈, 뺄셈, 곱셈, 나눗셈
-		int[] cntOper = new int[4];
-		
+		// 0 : + , 1 : - , 2 : × , 3 : ÷
+		int[] opCnt = new int[4];
+		char[] op = new char[N - 1];
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < 4; i++)
-			cntOper[i] = Integer.parseInt(st.nextToken());
-		
 		int idx = 0;
-		char[] opers = new char[N - 1];
 		for(int i = 0; i < 4; i++) {
-			while(cntOper[i] != 0) {
-				
+			opCnt[i] = Integer.parseInt(st.nextToken());
+			for(int j = 0; j < opCnt[i]; j++) {
 				switch(i) {
-				case 0 :
-					opers[idx++] = '+';
+				case 0:
+					op[idx++] = '+';
 					break;
-				case 1 :
-					opers[idx++] = '-';
+				case 1:
+					op[idx++] = '-';
 					break;
-				case 2 :
-					opers[idx++] = '*';
+				case 2:
+					op[idx++] = '*';
 					break;
-				case 3 :
-					opers[idx++] = '/';
+				case 3:
+					op[idx++] = '/';
 					break;
 				}
-				
-				cntOper[i]--;
 			}
 		}
 		
-		min = Integer.MAX_VALUE;
-		max = -Integer.MAX_VALUE;
+		perm(op, 0);
 		
-		perm(opers, 0);
 		System.out.println(max);
 		System.out.println(min);
 	}
 	
-	static long min;
-	static long max;
-	static int[] nums;
+	static int[] arr;
+	private static int max = Integer.MIN_VALUE;
+	private static int min = Integer.MAX_VALUE;
 	
-	static void Cal(char[] op) {
-		long solNum = nums[0];
-		
-		boolean flag = true;
-		for(int i = 0; i < op.length && flag; i++) {
-			switch(op[i]) {
-			case '+' :
-				solNum += nums[i + 1];
-				break;
-			case '-' :
-				solNum -= nums[i + 1];
-				break;
-			case '*' :
-				solNum *= nums[i + 1];
-				break;
-			case '/' :
-				solNum /= nums[i + 1];
-				break;
+	private static void perm(char[] op, int idx) {
+		if(op.length == idx) {
+			int sum = arr[0];
+			
+			for(int i = 0; i < op.length; i++) {
+				switch(op[i]) {
+				case '+' :
+					sum += arr[i + 1];
+					break;
+				case '-' :
+					sum -= arr[i + 1];
+					break;
+				case '*' :
+					sum *= arr[i + 1];
+					break;
+				case '/' :
+					if(sum < 0) {
+						sum = -sum;
+						sum /= arr[i + 1];
+						sum = -sum;
+					}
+					else {
+						sum /= arr[i + 1];
+					}
+					break;
+				}
 			}
-		}
-		
-		if(solNum < min)
-			min = solNum;
-		if(solNum > max)
-			max = solNum;
-	}
-	
-	static void perm(char[] opers, int idx) {
-		if(opers.length == idx) {
-			Cal(opers);
+			
+			max = Math.max(max, sum);
+			min = Math.min(min, sum);
+			
 			return;
 		}
 		
-		for(int i = idx; i < opers.length; i++) {
-			swap(opers, i, idx);
-			perm(opers, idx + 1);
-			swap(opers, i, idx);
+		for(int i = idx; i < op.length; i++) {
+			swap(op, i, idx);
+			perm(op, idx + 1);
+			swap(op, i, idx);
 		}
 	}
 	
-	static void swap(char[] op, int i, int j) {
+	private static void swap(char[] op, int i, int j) {
 		char tmp = op[i];
 		op[i] = op[j];
 		op[j] = tmp;
