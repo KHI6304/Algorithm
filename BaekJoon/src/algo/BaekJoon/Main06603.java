2683b808d170fe8b6d5886f5d2b819e98fdd36d3
김현일 <kimhi6304@naver.com>
2019-09-01 19:10:50 +0900

    로또

--- BaekJoon/src/algo/BaekJoon/Main06603.java
+++ BaekJoon/src/algo/BaekJoon/Main06603.java
@@ -10,39 +10,34 @@ public class Main06603 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		sb = new StringBuilder();
 		while(true) {
-			String inp = br.readLine();
-			if(inp.equals("0")) 
-				break;
+			st = new StringTokenizer(br.readLine());
+			int k = Integer.parseInt(st.nextToken());
 			
-			st = new StringTokenizer(inp);
-			int size = Integer.parseInt(st.nextToken());
-			int[] arr = new int[size];
+			if(k == 0) break;
 			
-			for(int i = 0; i < size; i++)
+			int[] arr = new int[k];
+			for(int i = 0; i < k; i++)
 				arr[i] = Integer.parseInt(st.nextToken());
 			
 			comb(arr, new int[6], 0, 0);
-			sb.append("\n");
+			System.out.println();
 		}
-		System.out.print(sb);
 	}
 	
-	static StringBuilder sb;
-	static void comb(int[] arr, int[] sel, int idx, int cnt) {
-		if(sel.length == cnt) {
+	private static void comb(int[] arr, int[] sel, int idx, int cnt) {
+		if(cnt == sel.length) {
 			for(int i = 0; i < sel.length; i++)
-				sb.append(sel[i]).append(" ");
-			sb.append("\n");
+				System.out.print(sel[i] + " ");
+			System.out.println();
 			return;
 		}
-		if(arr.length == idx)
+		
+		if(idx == arr.length)
 			return;
 		
 		sel[cnt] = arr[idx];
 		comb(arr, sel, idx + 1, cnt + 1);
-		sel[cnt] = 0;
 		comb(arr, sel, idx + 1, cnt);
 	}
 }
