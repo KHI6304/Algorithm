f45a60aa6fbf5f4d7ecffbaf7d8e052d761566b6
김현일 <kimhi6304@naver.com>
2020-02-07 14:36:30 +0900

    크로아티아 알파벳

---
+++ BaekJoon/src/algo/BaekJoon/Main02941.java
@@ -0,0 +1,30 @@
+package algo.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main02941 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] croAlp = {
+			"c=",
+			"c-",
+			"dz=",
+			"d-",
+			"lj",
+			"nj",
+			"s=",
+			"z="
+		};
+		
+		String inp = br.readLine();
+		int sol = inp.length();
+		
+		for(int i = 0; i < croAlp.length; i++) {
+			
+			
+		}
+	}
+}
