af29ef32681168ef2073400618b1cd8f94159c3a
김현일 <kimhi6304@naver.com>
2020-02-07 16:04:15 +0900

    크로아티아 알파벳

--- BaekJoon/src/algo/BaekJoon/Main02941.java
+++ BaekJoon/src/algo/BaekJoon/Main02941.java
@@ -5,13 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main02941 {
+	
+	/*
+	 * 크로아티아 알파벳을 
+	 * '*'이라는 하나의 문자로 치환.
+	 * 그리고 마지막에 해당 문자열의 길이를 출력한다.
+	 */
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String[] croAlp = {
+			"dz=",
 			"c=",
 			"c-",
-			"dz=",
 			"d-",
 			"lj",
 			"nj",
@@ -20,11 +27,10 @@ public class Main02941 {
 		};
 		
 		String inp = br.readLine();
-		int sol = inp.length();
 		
-		for(int i = 0; i < croAlp.length; i++) {
-			
-			
-		}
+		for(int i = 0; i < croAlp.length; i++)
+			inp = inp.replaceAll(croAlp[i], "*");
+		
+		System.out.println(inp.length());
 	}
 }
