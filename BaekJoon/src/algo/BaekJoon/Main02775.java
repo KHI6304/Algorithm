e6e00072f16904beda0945e0f08bfb6e3c039879
김현일 <kimhi6304@naver.com>
2020-02-07 16:35:07 +0900

    부녀회장이 될테야 (재귀 ver)

--- BaekJoon/src/algo/BaekJoon/Main02775.java
+++ BaekJoon/src/algo/BaekJoon/Main02775.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main02775 {
+	/*
+	 * 재귀로 푸는 방법과 이차원 배열에다가 값을 저장하는 단순 반복으로 푸는 방법이 있다.
+	 * 코드의 간결함은 재귀로 푸는 것이 월등히 좋지만,
+	 * 속도면에서는 후자와 5배 정도 차이가 난다.
+	 * 이를 통해 재귀는 속도 면에서 많은 손해가 생긴다는 것을 알 수 있었다.
+	 */
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -13,19 +20,17 @@ public class Main02775 {
 			int k = Integer.parseInt(br.readLine());
 			int n = Integer.parseInt(br.readLine());
 			
-			int[][] apart = new int[k + 1][n + 1];
-			for(int i = 1; i <= n; i++)
-				apart[0][i] = i;
 			
-			for(int i = 1; i <= k; i++) {
-				for(int j = 1; j <= n; j++) {
-					for(int a = 1; a <= j; a++) {
-						apart[i][j] += apart[i - 1][a];
-					}
-				}
-			}
-			
-			System.out.println(apart[k][n]);
+			System.out.println(solve(k, n));
 		}
+	}
+	
+	private static int solve(int k, int n) {
+		if(n == 0)
+			return 0;
+		else if(k == 0)
+			return n;
+		else
+			return solve(k, n - 1) + solve(k - 1, n);
 	}
 }
