79b5aec67b004c1fa345fe12f107046a41f8bec7
김현일 <kimhi6304@naver.com>
2020-02-12 00:06:09 +0900

    팩토리얼

---
+++ BaekJoon/src/algo/BaekJoon/Main10872.java
@@ -0,0 +1,21 @@
+package algo.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main10872 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(fact(N));
+	}
+	
+	private static int fact(int num) {
+		if(num <= 1)
+			return num;
+		
+		return num * fact(num - 1);
+	}
+}
