efc63ee6c261953dab96d99a03858b8aff8e5128
김현일 <kimhi6304@naver.com>
2020-02-11 23:37:33 +0900

    직사각형에서 탈출

---
+++ BaekJoon/src/algo/BaekJoon/Main01085.java
@@ -0,0 +1,23 @@
+package algo.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main01085 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		
+		int ans = Math.min(Math.min(x, y), Math.min(w - x, h - y));
+		System.out.println(ans);
+	}
+}
