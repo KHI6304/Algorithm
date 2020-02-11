614524a489d65118f9e0bc2768041ad6910e6736
김현일 <kimhi6304@naver.com>
2020-02-11 20:01:48 +0900

    손익분기점

---
+++ BaekJoon/src/algo/BaekJoon/Main01712.java
@@ -0,0 +1,27 @@
+package algo.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main01712 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		if(B >= C) {
+			System.out.println(-1);
+			return;
+		}
+		
+		int sol = A / (C - B) + 1;
+		System.out.println(sol);
+	}
+}
