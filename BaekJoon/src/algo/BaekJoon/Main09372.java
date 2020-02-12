6b30364168707871c8d125f6fd560102a1d34923
김현일 <kimhi6304@naver.com>
2020-02-12 21:45:17 +0900

    상근이의 여행

---
+++ BaekJoon/src/algo/BaekJoon/Main09372.java
@@ -0,0 +1,35 @@
+package algo.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main09372 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int tc = 0; tc < T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			int[][] map = new int[N + 1][N + 1];
+			for(int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+				map[a][b] = map[b][a] = 1;
+			}
+			
+			
+		}
+	}
+	
+	private static void BFS() {
+		
+	}
+}
