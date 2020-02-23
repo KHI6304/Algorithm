e1a875065805ccca9448a99778b6e8bdcfd648bc
KHI6304 <kimhi6304@naver.com>
2020-02-23 11:08:40 +0900

    N과 M (2)

---
+++ BaekJoon/src/algo/BaekJoon/Main15650.java
@@ -0,0 +1,50 @@
+package algo.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main15650 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		visited = new boolean[9];
+		sb = new StringBuilder();
+		DFS(new int[M], 0);
+		System.out.println(sb);
+	}
+	
+	private static int N;
+	private static int M;
+	private static boolean[] visited;
+	private static StringBuilder sb;
+	
+	private static void DFS(int[] arr, int depth) {
+		if(depth == M) {
+			for(int i = 1; i < arr.length; i++)
+				if(arr[i] < arr[i - 1])
+					return;
+			
+			for(int i = 0; i < M; i++)
+				sb.append(arr[i] + " ");
+			sb.append("\n");
+			
+			return;
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			if(visited[i]) continue;
+			
+			arr[depth] = i;
+			visited[i] = true;
+			DFS(arr, depth + 1);
+			visited[i] = false;
+		}
+	}
+}
