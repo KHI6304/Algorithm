e3ad89e532c7dc8c1590740db12ad21c070277fc
KHI6304 <kimhi6304@naver.com>
2020-02-27 13:55:52 +0900

    N과 M (6)

---
+++ BaekJoon/src/algo/BaekJoon/Main15655.java
@@ -0,0 +1,47 @@
+package algo.BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main15655 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		inp = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++)
+			inp[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(inp);
+		solve(new int[M], 0, 0);
+		System.out.println(sb);
+	}
+	
+	private static int N;
+	private static int M;
+	private static int[] inp;
+	private static StringBuilder sb = new StringBuilder();
+	
+	private static void solve(int[] arr, int idx, int depth) {
+		if(depth == M) {
+			for(int i = 0; i < arr.length; i++)
+				sb.append(arr[i] + " ");
+			sb.append("\n");
+			
+			return;
+		}
+		
+		for(int i = idx; i < N; i++) {
+			arr[depth] = inp[i];
+			solve(arr, i + 1, depth + 1);
+		}
+	}
+}
