07d4bf59835e21c346dc97fd8a0a9f1890464e74
KHI6304 <kimhi6304@naver.com>
2020-03-24 20:07:18 +0900

    가장 긴 증가하는 부분 수열 4

--- BaekJoon/src/algo/BaekJoon/Main14002.java
+++ BaekJoon/src/algo/BaekJoon/Main14002.java
@@ -3,51 +3,78 @@ package algo.BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main14002 {
+	private static class Node {
+		int idx, val;
+
+		public Node(int idx, int val) {
+			this.idx = idx;
+			this.val = val;
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
-		arr = new int[N];
-		dp = new int[N];
-		last = new int[N];
 		
-		int sol = 0;
-		int solIdx = 0;
+		int[] arr = new int[N];
+		int[] beforeIdx = new int[N];
+		
+		list = new ArrayList<>();
+		list.add(new Node(-1, Integer.MIN_VALUE));
+		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			dp[i] = 1;
-			last[i] = -1;
+			int size = list.size() - 1;
 			
-			for(int j = 0; j < i; j++) {
-				if(arr[i] > arr[j] && dp[i] <= dp[j]) {
-					dp[i] = dp[j] + 1;
-					last[i] = j;
-				}
+			if(arr[i] > list.get(size).val) {
+				beforeIdx[i] = list.get(size).idx;
+				list.add(new Node(i, arr[i]));
 			}
-			
-			if(sol < dp[i]) {
-				sol = dp[i];
-				solIdx = i;
+			else {
+				int idx = binarySearch(arr[i]);
+				list.get(idx).idx = i;
+				list.get(idx).val = arr[i];
+				beforeIdx[i] = list.get(idx - 1).idx;
 			}
 		}
 		
-		System.out.println(sol);
-		System.out.println(print(solIdx).trim());
+		int size = list.size() - 1;
+		System.out.println(size);
+		
+		Stack<Integer> stack = new Stack<>();
+		int idx = list.get(size).idx;
+		while(idx != -1) {
+			stack.push(arr[idx]);
+			idx = beforeIdx[idx];
+		}
+		
+		while(!stack.isEmpty())
+			System.out.print(stack.pop() + " ");
 	}
 	
-	private static int[] arr;
-	private static int[] dp;
-	private static int[] last;
+	private static ArrayList<Node> list;
 	
-	private static String print(int idx) {
-		if(idx == -1)
-			return "";
+	private static int binarySearch(int val) {
+		int left = 0;
+		int right = list.size() - 1;
 		
-		return print(last[idx]) + " " + arr[idx];
+		while(left < right) {
+			int mid = (left + right) / 2;
+			
+			if(val <= list.get(mid).val)
+				right = mid;
+			else
+				left = mid + 1;
+		}
+		
+		return right;
 	}
 }
