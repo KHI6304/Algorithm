b96d5077167a8754a300304d750b75c51560888e
KHI6304 <kimhi6304@naver.com>
2020-03-25 18:20:17 +0900

    개똥벌레 (부분합 풀이)

--- BaekJoon/src/algo/BaekJoon/Main03020.java
+++ BaekJoon/src/algo/BaekJoon/Main03020.java
@@ -3,13 +3,10 @@ package algo.BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main03020 {
-	private static int stoi(String s) {
-		return Integer.parseInt(s);
-	}
+	private static int stoi(String s) { return Integer.parseInt(s); }
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,24 +16,29 @@ public class Main03020 {
 		int N = Integer.parseInt(st.nextToken());
 		int H = Integer.parseInt(st.nextToken());
 		
-		int[] bot = new int[N / 2];
-		int[] top = new int[N / 2];
+		int[] bot = new int[H + 1];
+		int[] top = new int[H + 1];
 		
 		for(int i = 0; i < N / 2; i++) {
-			bot[i] = stoi(br.readLine());
-			top[i] = stoi(br.readLine());
+			bot[stoi(br.readLine())]++;
+			top[stoi(br.readLine())]++;
 		}
 		
-		Arrays.sort(bot);
-		Arrays.sort(top);
+		int[] botSum = new int[H + 1];
+		int[] topSum = new int[H + 1];
+		
+		for(int h = 1; h <= H; h++) {
+			botSum[h] = botSum[h - 1] + bot[h];
+			topSum[h] = topSum[h - 1] + top[h];
+		}
 		
 		int sol = N;
 		int solCnt = 0;
 		for(int h = 1; h <= H; h++) {
 			int cnt = 0;
 			
-			cnt += bot.length - lowerBound(bot, h);
-			cnt += top.length - lowerBound(top, H - h + 1);
+			cnt += botSum[H] - botSum[h - 1];
+			cnt += topSum[H] - topSum[H - h];
 			
 			if(cnt < sol) {
 				sol = cnt;
@@ -47,21 +49,5 @@ public class Main03020 {
 		}
 		
 		System.out.println(sol + " " + solCnt);
-	}
-	
-	private static int lowerBound(int[] arr, int h) {
-		int left = 0;
-		int right = arr.length;
-		
-		while(left < right) {
-			int mid = (left + right) / 2;
-			
-			if(h <= arr[mid])
-				right = mid;
-			else
-				left = mid + 1;
-		}
-		
-		return right;
 	}
 }
