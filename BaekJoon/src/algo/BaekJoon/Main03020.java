fade744c01b5904a36b52e499eec6b9e8a60f68f
KHI6304 <kimhi6304@naver.com>
2020-03-25 18:55:51 +0900

    개똥벌레 (lower_bound)

--- BaekJoon/src/algo/BaekJoon/Main03020.java
+++ BaekJoon/src/algo/BaekJoon/Main03020.java
@@ -3,11 +3,10 @@ package algo.BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main03020 {
-	private static int stoi(String s) { return Integer.parseInt(s); }
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -16,38 +15,49 @@ public class Main03020 {
 		int N = Integer.parseInt(st.nextToken());
 		int H = Integer.parseInt(st.nextToken());
 		
-		int[] bot = new int[H + 1];
-		int[] top = new int[H + 1];
+		int[] bot = new int[N / 2];
+		int[] top = new int[N / 2];
 		
 		for(int i = 0; i < N / 2; i++) {
-			bot[stoi(br.readLine())]++;
-			top[stoi(br.readLine())]++;
+			bot[i] = Integer.parseInt(br.readLine());
+			top[i] = Integer.parseInt(br.readLine());
 		}
 		
-		int[] botSum = new int[H + 1];
-		int[] topSum = new int[H + 1];
-		
-		for(int h = 1; h <= H; h++) {
-			botSum[h] = botSum[h - 1] + bot[h];
-			topSum[h] = topSum[h - 1] + top[h];
-		}
+		Arrays.sort(bot);
+		Arrays.sort(top);
 		
 		int sol = N;
 		int solCnt = 0;
 		for(int h = 1; h <= H; h++) {
 			int cnt = 0;
 			
-			cnt += botSum[H] - botSum[h - 1];
-			cnt += topSum[H] - topSum[H - h];
+			cnt += bot.length - lowerBound(bot, h);
+			cnt += top.length - lowerBound(top, H - h + 1);
 			
 			if(cnt < sol) {
 				sol = cnt;
 				solCnt = 1;
 			}
-			else if(cnt == sol)
+			else if(sol == cnt)
 				solCnt++;
 		}
 		
 		System.out.println(sol + " " + solCnt);
+	}
+	
+	private static int lowerBound(int[] arr, int val) {
+		int left = 0;
+		int right = arr.length;
+		
+		while(left < right) {
+			int mid = (left + right) / 2;
+			
+			if(val <= arr[mid])
+				right = mid;
+			else
+				left = mid + 1;
+		}
+		
+		return right;
 	}
 }
