7eff9669812bca2e159bd61b92c327d74562882b
KHI6304 <kimhi6304@naver.com>
2020-04-07 13:11:00 +0900

    피보나치 함수

--- BaekJoon/src/algo/BaekJoon/Main01003.java
+++ BaekJoon/src/algo/BaekJoon/Main01003.java
@@ -5,23 +5,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main01003 {
+	private static class Node {
+		int zero, one;
+
+		public Node(int zero, int one) {
+			this.zero = zero;
+			this.one = one;
+		}
+
+		@Override
+		public String toString() {
+			return zero + " " + one;
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());
-		System.out.println(fibo(N));
-	}
-	
-	static int[] memo = new int[50];
-	static {
-		memo[0] = 0;
-		memo[1] = 1;
-	}
-	
-	static int fibo(int n) {
-		if(n >= 2 && memo[n] == 0)
-			memo[n] = fibo(n - 1) + fibo(n - 2);
+		Node[] dp = new Node[41];
+		dp[0] = new Node(1, 0);
+		dp[1] = new Node(0, 1);
 		
-		return memo[n];
+		for(int i = 2; i < dp.length; i++)
+			dp[i] = new Node(dp[i - 2].zero + dp[i - 1].zero, dp[i - 2].one + dp[i - 1].one);
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int tc = 0; tc < T; tc++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			System.out.println(dp[num]);
+		}
 	}
 }
